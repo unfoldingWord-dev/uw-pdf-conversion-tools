f1bd01c25bee0dd5ba1a1a010a8f324db4c6fd84
Richard Mahn <rich.mahn@unfoldingword.org>
2020-02-26 14:42:22 -0500

    updates index.php

--- index.php
+++ index.php
@@ -43,7 +43,7 @@ foreach($dirs as $dir) {
             $filepath = './'.$dir.'/'.$file;
             $realfile = './'.$dir.'/'.basename(readlink($filepath));
 
-            echo '<a href="'.$realfile.'">'.$realfile.'</a> <em>('.date ("Y-m-d H:i:s", filemtime($filepath)).')</em><br/>'."\n";
+            echo '<a href="'.$realfile.'">'.basename($realfile).'</a> <em>('.date ("Y-m-d H:i:s", filemtime($filepath)).')</em><br/>'."\n";
         }
         echo "</p>\n";
     }
