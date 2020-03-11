0e839fe69b96b8502740ccd8d7a8f5db8427a8b0
Richard Mahn <richard_mahn@wycliffeassociates.org>
2020-03-11 20:27:10 +0000

    fix

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ date_default_timezone_set('US/Eastern');
 $dirs = array();
 $dir = opendir("."); // open the cwd..also do an err check.
 while(false != ($subdir = readdir($dir))) {
-        if(($subdir != ".") && ($subdir != "..") && is_dir($subdir) && ($subdir != "images") {
+        if(($subdir != ".") && ($subdir != "..") && is_dir($subdir) && ($subdir != "images")) {
                 $dirs[] = $subdir; // put in array.
         }
 }
