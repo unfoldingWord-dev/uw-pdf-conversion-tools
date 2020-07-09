e2e9159a27d3a252265eca49ad6403f4ef8939e1
Richard Mahn <richard_mahn@wycliffeassociates.org>
2020-07-09 17:05:15 +0000

    Adds shu locale

--- generate_all_pdfs.sh
+++ generate_all_pdfs.sh
@@ -13,11 +13,11 @@ export OUTPUT_DIR=/home/rmahn/output
 
 cd /home/rmahn/repos/uw-pdf-conversion-tools
 
-./obs_tn_pdf_converter.py -l en -l fr -l kn $@
-./obs_sn_pdf_converter.py -l en -l fr -l shu $@ 
-./obs_sn_sq_pdf_converter.py -l en -l fr -l shu $@ 
-./obs_sq_pdf_converter.py -l en -l fr -l shu $@ 
-./obs_pdf_converter.py -l en -l fr -l shu -l ml -l nag -l ta -l te -l kn -l as $@
+#./obs_tn_pdf_converter.py -l en -l fr -l kn $@
+#./obs_sn_pdf_converter.py -l en -l fr -l shu $@ 
+#./obs_sn_sq_pdf_converter.py -l en -l fr -l shu $@ 
+#./obs_sq_pdf_converter.py -l en -l fr -l shu $@ 
+#./obs_pdf_converter.py -l en -l fr -l shu -l ml -l nag -l ta -l te -l kn -l as $@
 ./tn_checking.py -l en $@
 ./tw_checking.py -l en $@
 ./ta_pdf_converter.py -l en $@
